9ced5ba55f7af739ec1e812694cc21a375331827
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-11-27 23:07:34 +0000

    [REVERTME] Revert "Merge tag 'LA.QSSI.14.0.r1-10300-qssi.0' of"
    This reverts commit b45af70d0de80f050c7694bb495589f3213efd3f, reversing
    changes made to da093853601ae914059e1866c12747c68cb64880.
    
    [1] - apparently it's breaking boot for some devices, and won't revert until we have a news blobs
    
    Change-Id: Ic65680761a436469e9febfe9d951b6ca2a7015dd

--- gralloc/QtiGralloc.cpp
+++ gralloc/QtiGralloc.cpp
@@ -27,13 +27,6 @@
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
- /*
- * Changes from Qualcomm Innovation Center are provided under the following license:
- *
- * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
- * SPDX-License-Identifier: BSD-3-Clause-Clear
- */
-
 #include "QtiGralloc.h"
 
 #include <log/log.h>
@@ -315,8 +308,6 @@ MetadataType getMetadataType(uint32_t in) {
       return MetadataType_TimedRendering;
     case QTI_CUSTOM_CONTENT_METADATA:
       return MetadataType_CustomContentMetadata;
-    case QTI_BUFFER_DEQUEUE_DURATION:
-      return MetadataType_BufferDequeueDuration;
     default:
       return MetadataType_Invalid;
   }
@@ -452,11 +443,6 @@ Error get(void *buffer, uint32_t type, void *param) {
     case QTI_CUSTOM_CONTENT_METADATA:
       err = decodeCustomContentMetadata(bytestream, param);
       break;
-    case QTI_BUFFER_DEQUEUE_DURATION:
-      err = static_cast<Error>(android::gralloc4::decodeInt64(
-          qtigralloc::MetadataType_BufferDequeueDuration, bytestream,
-          reinterpret_cast<int64_t *>(param)));
-      break;
     default:
       param = nullptr;
       return Error::UNSUPPORTED;
@@ -539,11 +525,6 @@ Error set(void *buffer, uint32_t type, void *param) {
       break;
     case QTI_CUSTOM_CONTENT_METADATA:
       err = encodeCustomContentMetadata(param, &bytestream);
-      break;
-    case QTI_BUFFER_DEQUEUE_DURATION:
-      err = static_cast<Error>(android::gralloc4::encodeInt64(
-          qtigralloc::MetadataType_BufferDequeueDuration,
-          *reinterpret_cast<int64_t *>(param), &bytestream));
       break;
     default:
       param = nullptr;
